0fee5fe962527a8e0130516709fd940b61dd2108
Marcel Unger <marcel@7meter17.de>
2019-10-09 12:36:28 +0200

    add Textfield to export add lib

--- lib/mdl/index.d.ts
+++ lib/mdl/index.d.ts
@@ -2,48 +2,16 @@
  * Created by ywu on 15/7/28.
  * FIXME: api-extractor error: Unable to analyze the export "default"...
  */
-import Button, {
-  AccentFab,
-  AccentRaisedButton,
-  ButtonStyles,
-  ColoredFab,
-  ColoredRaisedButton,
-  Fab,
-  FlatButton,
-  RaisedButton,
-} from './Button';
-
+import Button, { AccentFab, AccentRaisedButton, ButtonStyles, ColoredFab, ColoredRaisedButton, Fab, FlatButton, RaisedButton } from './Button';
 import Checkbox from './Checkbox';
 import IconToggle from './IconToggle';
 import RadioButton from './RadioButton';
 import RadioButtonGroup from './RadioButtonGroup';
 import Ripple from './Ripple';
-// @ts-ignore
 import Spinner from './Spinner';
 import Switch from './Switch';
 import Textfield from './Textfield';
-
-export {
-  AccentFab,
-  AccentRaisedButton,
-  Button,
-  ButtonStyles,
-  ColoredFab,
-  ColoredRaisedButton,
-  Fab,
-  FlatButton,
-  RaisedButton,
-  Checkbox,
-  IconToggle,
-  RadioButton,
-  RadioButtonGroup,
-  Ripple,
-  Spinner,
-  Switch,
-  Textfield
-};
-
-// Types
+export { AccentFab, AccentRaisedButton, Button, ButtonStyles, ColoredFab, ColoredRaisedButton, Fab, FlatButton, RaisedButton, Checkbox, IconToggle, RadioButton, RadioButtonGroup, Ripple, Spinner, Switch, Textfield };
 export { ButtonProps } from './Button';
 export { CheckboxProps } from './Checkbox';
 export { IconToggleProps } from './IconToggle';
@@ -52,3 +20,4 @@ export { RippleProps } from './Ripple';
 export { SpinnerProps } from './spinner_common';
 export { SwitchProps } from './Switch';
 export * from '../types';
+//# sourceMappingURL=index.d.ts.map
